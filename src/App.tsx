8d40a216d592ee5e4b3a19c0a07337887a56e6f4
gn-t-k <gntk1995422@gmail.com>
2020-11-22 10:47:12 +0900

    サンプルプロジェクトを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { FC } from 'react';
+
+import ColorfulBeads from 'containers/molecules/ColorfulBeads';
+import CounterBoard from 'containers/organisms/CounterBoard';
+
 import './App.css';
 
-const App: React.FC = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit
-        <code>src/App.tsx</code>
-        and save to reload.
-      </p>
-      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-        Learn React
-      </a>
+const App: FC = () => (
+  <div className="container">
+    <header>
+      <h1>ビーズカウンター</h1>
     </header>
+    <CounterBoard />
+    <ColorfulBeads />
   </div>
 );
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import { counterReducer, initialState } from 'reducer';
+import * as serviceWorker from './serviceWorker';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'semantic-ui-css/semantic.min.css';
+import './index.css';
+
+const store = createStore(counterReducer, initialState);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById('root') as HTMLElement,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+serviceWorker.unregister();
